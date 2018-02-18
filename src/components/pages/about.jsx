f4c545577cb4e54d44a1946acafcded914b4a73c
ryanpaixao <ryanpaixao@gmail.com>
2018-02-18 17:37:42 +0300

    Update text of about page.

--- src/components/pages/about.jsx
+++ src/components/pages/about.jsx
@@ -5,8 +5,15 @@ const About = () => {
     <div className="container-fluid">
       <h1>About</h1>
       <p>
-        Hello world! I'm Ryan Paixao, a Frontend Web Developer from South Florida.
-        
+        I'm a web developer who loves what I do. I get great enjoyment from creating something new. Starting with an empty directory, an empty file, a blank page, and to end up with a harmonious collaboration. It’s a great feeling. To have an idea, a concept, evolve and become something tangible, it is a powerful thing.
+      </p>
+
+      <p>
+        I am a life long learner. I love to learn, to learn how things work, why tasks are done the way they are, and if there’s a better/more efficient way to get it done.
+      </p>
+      
+      <p>
+        I am very grateful for being alive in a period of time where, so much of humanity’s collective information is so readily accessible. That it’s the easiest it’s ever been to build upon what others have done before you. To learn from their mistakes, and improve on their achievements. That different minds from different parts of the world can collaborate to create. 
       </p>
     </div>
   );
