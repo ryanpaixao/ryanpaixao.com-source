b54d037432691594cb09d6d5e0d475881f69957d
ryanpaixao <ryanpaixao@gmail.com>
2018-03-20 16:47:43 +0300

    Add deploy script to package.json

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+cd build
+echo $(pwd)
+git init
+# echo "www.ryanpaixao.com" >> CNAME
+git add .
+git commit -m "Push build $(date)"
+git remote add origin "https://github.com/ryanpaixao/ryanpaixao.github.io.git"
+git push -f origin master
