06389ee0a7cc69a5b22137011dcb6e328f0ad6be
ryanpaixao <ryanpaixao@gmail.com>
2018-03-20 18:03:14 +0300

    Update About page, center content on all pages.

--- src/components/pages/about.jsx
+++ src/components/pages/about.jsx
@@ -32,16 +32,27 @@ const About = () => {
       <div className='text-body'>
         <h1 className='header-title'>About The Site</h1>
         <p>
-          I used <a href='https://github.com/facebook/create-react-app'>create-react-app</a> to make this site. 
-          I find that it's the quickest, and easiest route to a finished Reactjs product.
+          I used <a href='https://github.com/facebook/create-react-app' target='_blank' rel='noopener noreferrer'>create-react-app</a> to make this site. 
+          I found that it was the quickest, and easiest route to a finished Reactjs product.
         </p>
         <br />
         <p>
-          For animations, <a href='https://github.com/reactjs/react-transition-group'>react-transition-group</a> was used.
+          <span><a href='https://reacttraining.com/react-router/' target='_blank' rel='noopener noreferrer'>React router</a> provides the routing for the different pages. </span> 
         </p>
         <br />
         <p>
-          Feel free to check out the source code for yourself at <a href='https://github.com/ryanpaixao/ryanpaixao.com-source'>ryanpaixao-source</a>
+          <a href='https://www.w3schools.com/css/css_grid.asp' target='_blank' rel='noopener noreferrer'>Css-grid</a> was used to make the site responsive, which means that it adapts to the user's device. 
+          Whether, it viewed on desktop, tablet, or mobile screen. 
+        </p>
+        <br />
+        <p>
+          For animations, <a href='https://github.com/reactjs/react-transition-group' target='_blank' rel='noopener noreferrer'>react-transition-group</a> was utilized. 
+          React-transition-group allows for animations to be done purely with css-transitions. 
+          This prevents direct DOM manipulation, as to not interfere with React, and have it be in sync with the state of the components. 
+        </p>
+        <br />
+        <p>
+          Feel free to check out the source code for yourself at <a href='https://github.com/ryanpaixao/ryanpaixao.com-source' target='_blank' rel='noopener noreferrer'>ryanpaixao-source</a>
         </p>
       </div>
       
